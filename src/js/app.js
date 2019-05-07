f30f5bef017e0fd9e6de66a0ac66e3049e290258
Nelson Diaz <abnerdc86@gmail.com>
2019-05-07 18:07:58 -0400

    setting up webpack #1

--- src/js/app.js
+++ src/js/app.js
@@ -6,15 +6,18 @@
 			$(this).parent().addClass('active').siblings().removeClass('active')	
 		})
 })*/
-$(function(){
-		$(function(){$("#crYear").text(new Date().getFullYear())});
+$(function () {
+    $(function () { $("#crYear").text(new Date().getFullYear()) });
 })
-	
-<!-- Script to load the images -->
-    var text = "";
+
+// Script to load the images
+function loadImages() {
+    var tag = "";
     var i;
     for (i = 1; i < 67; i++) {
-        text += '<img src="gallery/img-' + i +'.jpg">';
+        tag += '<img src="gallery/img-' + i + '.jpg">';
     }
+    return tag
+}
 
-    document.getElementById("gallery-1").innerHTML = text;
+document.getElementById("gallery-1").innerHTML = loadImages();
