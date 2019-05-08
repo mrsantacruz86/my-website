6c9eff64d28d328964133522ba614fe136769330
Nelson Diaz <abnerdc86@gmail.com>
2019-05-08 18:13:32 -0400

    working on webpack  #3

--- src/js/app.js
+++ src/js/app.js
@@ -5,25 +5,25 @@ import jquery from 'jquery';
 window.$ = jquery;
 
 // jQuery methods
-$(function(){
-		$('.nav li').filter(function(){return this.href==location.href}).parent().addClass('active').siblings().removeClass('active')
-		$('.nav li').click(function(){
-			$(this).parent().addClass('active').siblings().removeClass('active')	
-		})
+$(function () {
+  $('.nav li').filter(function () { return this.href == location.href }).parent().addClass('active').siblings().removeClass('active')
+  $('.nav li').click(function () {
+    $(this).parent().addClass('active').siblings().removeClass('active')
+  })
 })
 $(function () {
-    $(function () { $("#crYear").text(new Date().getFullYear()) });
+  $(function () { $("#crYear").text(new Date().getFullYear()) });
 })
 
 
 // Script to load the images
 function loadImages() {
-    var tag = "";
-    var i;
-    for (i = 1; i < 67; i++) {
-        tag += '<img src="gallery/img-' + i + '.jpg">';
-    }
-    return tag
+  var tag = "";
+  var i;
+  for (i = 1; i < 67; i++) {
+    tag += '<img src="gallery/img-' + i + '.jpg">';
+  }
+  return tag
 }
 
 // document.getElementById("gallery-1").innerHTML = loadImages();
--- webpack.config.js
+++ webpack.config.js
@@ -3,48 +3,39 @@ const webpack = require("webpack");
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var extractPlugin = new ExtractTextPlugin({
-	filename: "main.css"
+  filename: "main.css"
 })
 
 module.exports = {
-	entry: "./src/js/app.js",
-	output:{
-		path:path.resolve(__dirname,"dist"),
-		filename: "bundle.js",
-		publicPath: "/dist"
-	},
-	module:{
-		rules: [
-			{
-				test:/\.scss$/,
-				//Webpack will execute these loaders in reverse order. So the order matters.
-				use:[
-					extractPlugin.extract({
-						use:[
-							"css-loader",
-							"scss-loader"
-						]
-					}),
-					"css-loader"
-				] 
-			},
-			{
-				test: /\.js$/,
-				use:[
-					{
-						loader: "babel-loader",
-						options: {
-							presets: ["es2015"]
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins:[
-		extractPlugin
-		// new webpack.optimize.UglifyJsPlugin({
-		// 	//...
-		// })
-	]
+  mode: "development",
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["es2015"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: extractPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    extractPlugin
+  ]
 }
