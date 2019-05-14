08f9d7fd56a6c7a967752c7e2f88e2b2a87c8c6a
Nelson Diaz <abnerdc86@gmail.com>
2019-05-14 18:17:00 -0400

    Deployment test

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,8 @@ module.exports = {
   },
   output: {
     filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    publicPath: "./dist"
+    path: path.resolve(__dirname, "./docs"),
+    publicPath: "./docs"
   },
   module: {
     rules: [
